31c4dc7659ab0ac1ba29f148104c63f494ed281e
jinmingteo <jinmingteo95@gmail.com>
2017-07-13 00:50:29 +0800

    misc edits

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,12 +1,12 @@
 @extends('main')
 
-@section('title', '| Blog')
+@section('title', '| Designs')
 
 @section('content')
 
 	<div class ="row">
 		<div class = "col-md-8 col-md-offset-2">
-			<h1>Blog</h1>
+			<h1> Designs</h1>
 		</div>
 	</div>
 
