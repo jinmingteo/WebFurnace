32701e49c6035617eb8e9045b9b4ea1b1bdf0a01
jinmingteo <jinmingteo95@gmail.com>
2017-07-25 00:04:53 +0800

    safey catch

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,22 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-  @include('partials._head')
-  </head>
+    <head>
+    @include('partials._head')
+    </head>
 
-  <body>
-  @include('partials._nav')
+    <body>
+    @include('partials._nav')
 
-    <div class = "container">
-      @include('partials._messages')
-      
-      @yield('content')
-      @include('partials._footer')
-    </div>
+      <div class = "container">
+        @include('partials._messages')
+        
+        @yield('content')
+        @include('partials._footer')
+      </div>
 
-      @include('partials._javascript')
+        @include('partials._javascript')
 
-      @yield('scripts')
-  </body>
-
+        @yield('scripts')
+    </body>
+  }
 </html>
