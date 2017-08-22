420c460cc8dc026496a40df27f7422bc7621050a
jinmingteo <jinmingteo95@gmail.com>
2017-08-22 22:56:27 +0800

    register -> update profile

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/designerprofile';
 
     /**
      * Create a new controller instance.
--- resources/views/designer/profile/single.blade.php
+++ resources/views/designer/profile/single.blade.php
@@ -56,49 +56,5 @@
 <br>
 <br>
 
-<!--<div class="container">
 
-	<div class="row">
-		<div class="col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
-    	 <div class="well profile">
-            <div class="col-sm-12">
-                <div class="col-xs-12 col-sm-8">
-                    <h2>{{$user -> username}}</h2> 
-                    <p><strong>About: </strong> {{$design-> aboutme}}</p>
-                    <div class="row">
-                        <div class="col-md-11">
-                            <p><strong>Email: {{$user -> email}}</strong> 
-                            </p>
-                        </div>
-                        @if (Auth::user()->username == $user->username)
-                        <div class="col-md-1">
-                            <a href="{{ route('designerprofile.edit', $user->username) }}" class="btn btn-primary" style="margin-bottom: 5px">Update Profile</a>
-                        </div>
-                        @endif
-                    </div>
-                </div> 
-                <div class="col-xs-12 col-sm-4 text-center">
-                    <figure>
-                        <img src="{{asset('images/'.$design->image)}}"/>
-                    </figure>
-                </div>
-            </div>            
-            <div class="col-xs-12 divider text-center">
-                <div class="col-xs-12 col-sm-4 emphasis">
-                    <p><small>Created At:</small></p>
-                    <h2><strong>{{ date('M j, Y ',strtotime($user-> created_at))}}  </strong></h2>                    
-                </div>
-                <div class="col-xs-12 col-sm-4 emphasis">
-                    <p><small>Job Request On Hand</small></p>
-                    <h2><strong>0</strong></h2>                    
-                </div>
-                <div class="col-xs-12 col-sm-4 emphasis">
-                    <p><small>Completed Job Request</small></p>
-                    <h2><strong>0</strong></h2>                    
-                </div>
-            </div>
-    	 </div>                 
-		</div>
-	</div>
-</div>-->
 @endsection
